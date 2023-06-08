7e9e3fea7da1eb510c56a976fe25d2130d0c1df5
esraac <102144531+esraac@users.noreply.github.com>
2023-06-08 15:30:11 +0300

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        ATM atm= new ATM();
-        Account hesap= new Account("Esra","123java",1750.75);
-        atm.calis(hesap);
-        System.out.println("Programdan çıkılıyor...");
-
-    }
-}
